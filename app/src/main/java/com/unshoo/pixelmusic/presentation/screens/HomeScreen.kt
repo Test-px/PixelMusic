0cfcc8fc32b263780a2705c3b90bfab689b6c022
saurav-home <bookjangle65@gmail.com>
2026-08-08 07:29:29 +0530

    Update HomeScreen.kt

--- app/src/main/java/com/unshoo/pixelmusic/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/unshoo/pixelmusic/presentation/screens/HomeScreen.kt
@@ -288,7 +288,7 @@ fun HomeScreen(
     val bottomPadding = if (currentSong != null) MiniPlayerHeight else 0.dp
     // Bring bottomPadding back into the formula
     val fabBottomPadding by animateDpAsState(
-    targetValue = paddingValuesParent.calculateBottomPadding() + bottomPadding + 16.dp,
+    targetValue = paddingValuesParent.calculateBottomPadding() + bottomPadding + 20.dp,
     animationSpec = spring(
         dampingRatio = Spring.DampingRatioMediumBouncy,
         stiffness = Spring.StiffnessLow
