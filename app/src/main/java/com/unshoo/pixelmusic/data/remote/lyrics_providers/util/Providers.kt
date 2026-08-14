c9d41ba05120f554fa1703285986d84dacf07d45
atappu805 <atappu805@gmail.com>
2026-08-14 20:33:56 +0530

    Create Providers.kt

---
+++ app/src/main/java/com/unshoo/pixelmusic/data/remote/lyrics_providers/util/Providers.kt
@@ -0,0 +1,12 @@
+package com.unshoo.pixelmusic.data.remote.lyrics_providers.util
+
+enum class Providers {
+    SPOTIFY,
+    LRCLIB,
+    NETEASE,
+    QQMUSIC,
+    APPLE,
+    LYRICSPLUS,
+    BETTERLYRICS
+}
+
