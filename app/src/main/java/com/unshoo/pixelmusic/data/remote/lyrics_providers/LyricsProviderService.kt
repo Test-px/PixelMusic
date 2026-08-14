fc939f5d00260315ec3e23cfa2452bdffe229f6b
atappu805 <atappu805@gmail.com>
2026-08-14 21:27:02 +0530

    Update LyricsProviderService.kt

--- app/src/main/java/com/unshoo/pixelmusic/data/remote/lyrics_providers/LyricsProviderService.kt
+++ app/src/main/java/com/unshoo/pixelmusic/data/remote/lyrics_providers/LyricsProviderService.kt
@@ -1,5 +1,6 @@
 package com.unshoo.pixelmusic.data.remote.lyrics_providers
 
+import com.unshoo.pixelmusic.data.remote.lyrics_providers.apple.AppleAPI
 import com.unshoo.pixelmusic.data.remote.lyrics_providers.model.SongInfo
 import com.unshoo.pixelmusic.data.remote.lyrics_providers.others.*
 import com.unshoo.pixelmusic.data.remote.lyrics_providers.spotify.SpotifyAPI
@@ -9,12 +10,13 @@ import okhttp3.OkHttpClient
 
 class LyricsProviderService(client: OkHttpClient) {
     
-    private val lrcLibAPI = LRCLibAPI(client)
-    private val neteaseAPI = NeteaseAPI(client)
-    private val spotifyAPI = SpotifyAPI(client)
-    private val qqMusicAPI = QQMusicAPI(client)
-    private val lyricsPlusAPI = LyricsPlusAPI(client)
-    private val betterLyricsAPI = BetterLyricsAPI(client)
+    val lrcLibAPI = LRCLibAPI(client)
+    val neteaseAPI = NeteaseAPI(client)
+    val spotifyAPI = SpotifyAPI(client)
+    val qqMusicAPI = QQMusicAPI(client)
+    val appleAPI = AppleAPI(client)
+    val lyricsPlusAPI = LyricsPlusAPI(client)
+    val betterLyricsAPI = BetterLyricsAPI(client)
 
     suspend fun getSongInfo(query: SongInfo, offset: Int = 0, provider: Providers): SongInfo? {
         return try {
@@ -23,25 +25,34 @@ class LyricsProviderService(client: OkHttpClient) {
                 Providers.NETEASE -> neteaseAPI.getSongInfo(query, offset) ?: throw NoTrackFoundException()
                 Providers.SPOTIFY -> spotifyAPI.getSongInfo(query, offset) ?: throw NoTrackFoundException()
                 Providers.QQMUSIC -> qqMusicAPI.getSongInfo(query, offset) ?: throw NoTrackFoundException()
+                Providers.APPLE -> appleAPI.getSongInfo(query, offset) ?: throw NoTrackFoundException()
                 Providers.LYRICSPLUS, Providers.BETTERLYRICS ->
                     if (offset > 0) throw NoTrackFoundException()
                     else SongInfo(songName = query.songName, artistName = query.artistName)
-                else -> throw InternalErrorException("Provider not implemented yet")
             }
         } catch (e: Exception) {
             throw e
         }
     }
 
-    suspend fun getSyncedLyrics(song: SongInfo, provider: Providers): String? {
+    suspend fun getSyncedLyrics(
+        song: SongInfo,
+        provider: Providers,
+        includeTranslationNetEase: Boolean = false,
+        includeRomanizationNetEase: Boolean = false
+    ): String? {
         return when (provider) {
             Providers.LRCLIB -> lrcLibAPI.getSyncedLyrics(song.lrcLibID ?: 0)
-            Providers.NETEASE -> neteaseAPI.getSyncedLyrics(song.neteaseID ?: 0L)
+            Providers.NETEASE -> neteaseAPI.getSyncedLyrics(
+                song.neteaseID ?: 0L,
+                includeTranslationNetEase,
+                includeRomanizationNetEase
+            )
             Providers.SPOTIFY -> spotifyAPI.getSyncedLyrics(song.songLink ?: "")
             Providers.QQMUSIC -> qqMusicAPI.getSyncedLyrics(song.qqPayload ?: "")
+            Providers.APPLE -> appleAPI.getSyncedLyrics(song.appleID ?: 0L)
             Providers.LYRICSPLUS -> lyricsPlusAPI.getSyncedLyrics(song.songName.orEmpty(), song.artistName.orEmpty())
             Providers.BETTERLYRICS -> betterLyricsAPI.getSyncedLyrics(song.songName.orEmpty(), song.artistName.orEmpty())
-            else -> null
         }
     }
 }
