26424f1d0181ae2fbf262c2967e5a37355627a6e
atappu805 <atappu805@gmail.com>
2026-08-23 17:52:10 +0530

    Update OptimizedAlbumArt.kt

--- app/src/main/java/com/unshoo/pixelmusic/presentation/components/OptimizedAlbumArt.kt
+++ app/src/main/java/com/unshoo/pixelmusic/presentation/components/OptimizedAlbumArt.kt
@@ -83,9 +83,9 @@ fun OptimizedAlbumArt(
     SmartImageCache.albumArtQualityWifi
 }
 
-    val optimizedUri = remember(uri, albumArtQuality) {
+    val optimizedUri = remember(uri, effectiveQuality) { // UPDATE: Re-key to effectiveQuality
         if (uri is String && (uri.contains("ggpht.com") || uri.contains("googleusercontent.com"))) {
-            val size = if (albumArtQuality.maxSize > 0) albumArtQuality.maxSize else 1200
+            val size = if (effectiveQuality.maxSize > 0) effectiveQuality.maxSize else 1200 // UPDATE: Use effectiveQuality
             uri.replace(Regex("=w\\d+-h\\d+"), "=w$size-h$size")
                .replace(Regex("=s\\d+"), "=s$size")
         } else {
