09cbf0d9806005d31ceb067ce8d91eb194444274
atappu805 <atappu805@gmail.com>
2026-08-31 20:59:51 +0530

    Update YouTubeClient.kt

--- app/src/main/java/unshoo/ianshulyadav/pixelmusic/innertube/models/YouTubeClient.kt
+++ app/src/main/java/unshoo/ianshulyadav/pixelmusic/innertube/models/YouTubeClient.kt
@@ -1,95 +1,43 @@
-/*
- * ArchiveTune (2026)
- * © Chartreux Westia — github.com/ianshulyadav
- * GPL-3.0 License | Contributors: see git history
- * Do not remove or alter this notice. - Per GPL-3.0 Section 4 & Section 5
- */
-
-
-
-
-
-package unshoo.ianshulyadav.pixelmusic.innertube.models
+package com.unshoo.pixelmusic.data.remote.youtube
 
 import kotlinx.serialization.Serializable
-import java.util.Locale
 
 @Serializable
 data class YouTubeClient(
     val clientName: String,
     val clientVersion: String,
-    val clientId: String,
+    val clientId: String? = null,
+    val api_key: String? = null,
     val userAgent: String,
     val osName: String? = null,
     val osVersion: String? = null,
-    val deviceMake: String? = null,
-    val deviceModel: String? = null,
-    val androidSdkVersion: String? = null,
-    val buildId: String? = null,
-    val cronetVersion: String? = null,
-    val packageName: String? = null,
-    val friendlyName: String? = null,
+    val referer: String? = null,
     val loginSupported: Boolean = false,
-    val loginRequired: Boolean = false,
     val useSignatureTimestamp: Boolean = false,
-    val isEmbedded: Boolean = false,
 ) {
-    fun toContext(locale: YouTubeLocale, visitorData: String?, dataSyncId: String?) = Context(
-        client = Context.Client(
-            clientName = clientName,
-            clientVersion = clientVersion,
-            osName = osName,
-            osVersion = osVersion,
-            deviceMake = deviceMake,
-            deviceModel = deviceModel,
-            androidSdkVersion = androidSdkVersion,
-            gl = locale.gl,
-            hl = locale.hl,
-            visitorData = visitorData
-        ),
-        user = Context.User(
-            onBehalfOfUser = if (loginSupported) dataSyncId else null
-        ),
-    )
-
-    fun requestOrigin(): String {
-        return when (clientName.uppercase(Locale.US)) {
-            "TVHTML5", "TVHTML5_SIMPLY_EMBEDDED_PLAYER", "TVHTML5_SIMPLY" -> ORIGIN_YOUTUBE
-            else -> ORIGIN_YOUTUBE_MUSIC
-        }
-    }
-
-    fun requestReferer(): String {
-        return when (clientName.uppercase(Locale.US)) {
-            "TVHTML5", "TVHTML5_SIMPLY_EMBEDDED_PLAYER", "TVHTML5_SIMPLY" -> REFERER_YOUTUBE_TV
-            else -> REFERER_YOUTUBE_MUSIC
-        }
-    }
-
     companion object {
-        const val USER_AGENT_WEB = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
+        const val USER_AGENT_WEB =
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
 
         const val ORIGIN_YOUTUBE_MUSIC = "https://music.youtube.com"
         const val REFERER_YOUTUBE_MUSIC = "$ORIGIN_YOUTUBE_MUSIC/"
         const val API_URL_YOUTUBE_MUSIC = "$ORIGIN_YOUTUBE_MUSIC/youtubei/v1/"
 
-        const val ORIGIN_YOUTUBE = "https://www.youtube.com"
-        const val REFERER_YOUTUBE_TV = "$ORIGIN_YOUTUBE/tv"
-
         val WEB = YouTubeClient(
             clientName = "WEB",
-            clientVersion = "2.20260114.00.00",
+            clientVersion = "2.20250312.04.00",
             clientId = "1",
             userAgent = USER_AGENT_WEB,
         )
 
         val WEB_REMIX = YouTubeClient(
             clientName = "WEB_REMIX",
-            clientVersion = "1.20260114.01.00",
+            clientVersion = "1.20260304.03.00",
             clientId = "67",
             userAgent = USER_AGENT_WEB,
+            referer = REFERER_YOUTUBE_MUSIC,
             loginSupported = true,
             useSignatureTimestamp = true,
         )
     }
-    
+}
