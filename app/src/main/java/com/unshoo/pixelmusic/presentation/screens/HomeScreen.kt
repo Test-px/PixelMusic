c60dd82dbb5dd6a4a30b55182b90327a30ca8ec3
atappu805 <atappu805@gmail.com>
2026-09-03 04:58:46 +0530

    Update HomeScreen.kt

--- app/src/main/java/com/unshoo/pixelmusic/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/unshoo/pixelmusic/presentation/screens/HomeScreen.kt
@@ -399,7 +399,7 @@ fun HomeScreen(
     LaunchedEffect(Unit) {
         if (!isColdLaunchSweepPlayed) {
             isColdLaunchSweepPlayed = true
-            delay(350L)
+            delay(370L)
             triggerColdLaunchSweep = true
         }
     }
